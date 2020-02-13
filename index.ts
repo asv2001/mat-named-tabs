4c5a589377760bbd8481e9195ceaf89f312f4afa
Anton Sinelnyk <asinelnyk@evolutiongaming.com>
2020-02-13 11:25:47 +0100

    Adds Angular 9 support

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { MatTabsModule } from "@angular/material";
+import { MatTabsModule } from "@angular/material/tabs";
 import { NgModule } from "@angular/core";
 import { SelectedTabNameDirective } from "./selected-tab-name.directive/selected-tab-name.directive";
 import { TabNameDirective } from "./tab-name.directive/tab-name.directive";
